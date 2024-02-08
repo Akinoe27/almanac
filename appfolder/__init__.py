45ccd712d27f9ea814cde240e907319665c4e324
Akinoe27 <elder_aquino27@yahoo.com>
2024-02-08 12:07:45 +0800

    update

--- appfolder/__init__.py
+++ appfolder/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
 app.config['MYSQL_PASSWORD'] = ""
-app.config['MYSQL_DB'] = "almanac"
+app.config['MYSQL_DB'] = "almanac_db"
 
 mysql = MySQL(app)
 
